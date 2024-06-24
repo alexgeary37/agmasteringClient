b0bc98446c55b8584b87caf34208d40b72a68194
alexgeary37 <alexgeary37@gmail.com>
2024-06-24 14:32:10 +1200

    Comment out onClick in StartAProject while Stripe isn't live

--- src/components/StartAProject.jsx
+++ src/components/StartAProject.jsx
@@ -125,15 +125,15 @@ export default function StartAProject() {
               <Button
                 fullWidth
                 variant={tier.buttonVariant}
-                onClick={() => {
-                  if (tier.buttonText === "START MIXING") {
-                    navigate("/start-a-project/mixing");
-                  } else if (tier.buttonText === "START MASTERING") {
-                    navigate("/start-a-project/mastering");
-                  } else {
-                    navigate("/start-a-project/mix&master");
-                  }
-                }}
+                // onClick={() => {
+                //   if (tier.buttonText === "START MIXING") {
+                //     navigate("/start-a-project/mixing");
+                //   } else if (tier.buttonText === "START MASTERING") {
+                //     navigate("/start-a-project/mastering");
+                //   } else {
+                //     navigate("/start-a-project/mix&master");
+                //   }
+                // }}
               >
                 {tier.buttonText}
               </Button>
